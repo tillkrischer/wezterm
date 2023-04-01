0b78d8a859f48b41eb69fa5be99219affd44e2f0
Till Krischer <till@krischer.io>
2023-04-01 09:56:32 +0200

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,4 +4,5 @@ config.font = wezterm.font 'JetBrains Mono'
 config.color_scheme = 'Catppuccin Latte'
 config.enable_tab_bar = false
 config.harfbuzz_features = {"calt=0", "clig=0", "liga=0"}
+config.default_prog = { 'C:/Program Files/PowerShell/7/pwsh.exe' }
 return config

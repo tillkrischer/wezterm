c15aef70e36b93ae315cf4034b30ea09ea8a184f
Till Krischer <till@krischer.io>
2024-07-22 23:04:13 +0200

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,8 +1,10 @@
 local wezterm = require 'wezterm'
 local config = {}
 config.font = wezterm.font 'JetBrains Mono'
-config.color_scheme = 'Catppuccin Latte'
-config.enable_tab_bar = false
+config.color_scheme = "Tokyo Night"
+-- config.color_scheme = 'Catppuccin Latte'
+-- config.enable_tab_bar = false
+config.use_fancy_tab_bar = false
 config.harfbuzz_features = {"calt=0", "clig=0", "liga=0"}
 config.default_prog = { 'C:/Program Files/PowerShell/7/pwsh.exe' }
 config.keys = {
